01f6d2b0d576d3d469824fc4d42f47e8e61093fb
Philip-21 <philipuzomaobiora@gmail.com>
2023-06-23 07:54:15 +0100

    websockets for backend
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

---
+++ backend/helpers/websockets.go
@@ -0,0 +1,61 @@
+package helpers
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+
+// sends messages from the server to all connected clients
+var broadcast = make(chan []byte)
+
+var upgrader = websocket.Upgrader{} 
+
+
+func HandleWebsocket(c*gin.Context){
+	//upgrades the HTTP server connection to the WebSocket protocol.
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Error in Upgrading websocket connection", err)
+		return
+	}
+	clients[conn] = true 
+
+	defer func() {
+		conn.Close()
+		delete(clients, conn)
+	}()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read Websocket error", err)
+			break
+		}
+		broadcast <- message
+	}
+
+}
+
+
+
+// A function that continiousy listens to the broadcst channel
+func HandeBroadcast() {
+	for {
+		message := <-broadcast
+
+		for client := range clients {
+			// iterates over all connected clients and sends the message
+			//to each client using the client.WriteMessage method
+			err := client.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				log.Println("Writer websocket error", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
