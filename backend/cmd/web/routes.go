0733792cc2a8a631bfcd1b093d7d7bb80fa5bdbf
Philip-21 <philipuzomaobiora@gmail.com>
2023-06-24 10:28:48 +0100

    backend websockets
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- backend/cmd/web/routes.go
+++ backend/cmd/web/routes.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"wallet-api/handlers"
+	"wallet-api/helpers"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Routes (app*handlers.Repository)*gin.Engine{
+func Routes(app *handlers.Repository) *gin.Engine {
 
 	router := gin.Default()
 
-	return router 
+	//register websocket handlers 
+	router.GET("/ws",helpers.HandleWebsocket)
+	//start the broadcast routine
+	go helpers.HandleBroadcast()
+
+	router.Use(gin.Logger())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https//*", "http://*"},//communicatng with port 3000 enabled
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Access-Control-Request-Method", "Access-Control-Request-Headers", "Accept", "Authorization", " Accept-Encoding",
+			"Content-Type", "Connection", " Host", "Origin", "User-Agent", "Referer", "Cache-Control", "X-header", "Token", "X-CSRF-Token"},
+		ExposeHeaders:    []string{"Link", "Content-Length"},
+		AllowCredentials: true, //AllowCredentials indicates whether the request can include user credentials like cookies
+
+		MaxAge: 300, //MaxAge indicates how long (with second-precision) the results of a preflight request can be cached
+	}))
+
+	return router
 
 }
